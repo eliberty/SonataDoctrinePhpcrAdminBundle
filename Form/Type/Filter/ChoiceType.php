90e075590d74466aaea2bfe7d37e33c3dfe6a668
BENARD Patrick <pbeliberty@users.noreply.github.com>
2017-07-21 16:35:02 +0200

    Filter Choice Type for SF3

--- Form/Type/Filter/ChoiceType.php
+++ Form/Type/Filter/ChoiceType.php
@@ -48,6 +48,11 @@ class ChoiceType extends BaseChoiceType
             self::TYPE_CONTAINS_WORDS => $this->translator->trans('label_type_contains_words', array(), 'SonataDoctrinePHPCRAdmin'),
         );
 
+        if (method_exists('Symfony\Component\Form\FormTypeInterface', 'setDefaultOptions')) {
+            $choices = array_flip($choices);
+        }
+        
+        
         $builder
             ->add('type', 'choice', array('choices' => $choices, 'required' => false))
             ->add('value', $options['field_type'], array_merge(array('required' => false), $options['field_options']))
