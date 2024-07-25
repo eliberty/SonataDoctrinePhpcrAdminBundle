42b116039ca6daac8d9e28f78f89543b8cf34cf2
Maxime <maxime.brignon@eliberty.fr>
2024-07-25 11:27:30 +0200

    fix: upgrade sonata 4 typeGuesser

--- src/Guesser/TypeGuesser.php
+++ src/Guesser/TypeGuesser.php
@@ -13,14 +13,9 @@ declare(strict_types = 1);
 
 namespace Sonata\DoctrinePHPCRAdminBundle\Guesser;
 
-use Doctrine\Bundle\PHPCRBundle\ManagerRegistry;
 use Doctrine\ODM\PHPCR\Mapping\ClassMetadata;
 use Sonata\AdminBundle\FieldDescription\FieldDescriptionInterface;
 use Sonata\AdminBundle\FieldDescription\TypeGuesserInterface;
-use Sonata\Form\Type\BooleanType;
-use Sonata\Form\Type\DatePickerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Guess\Guess;
 use Symfony\Component\Form\Guess\TypeGuess;
 
@@ -31,50 +26,39 @@ use Symfony\Component\Form\Guess\TypeGuess;
  */
 class TypeGuesser implements TypeGuesserInterface
 {
-    protected ManagerRegistry $registry;
-
-    public function __construct(ManagerRegistry $registry)
-    {
-        $this->registry = $registry;
-    }
-
-    /**
-     * TODO: test new implementation.
-     */
     public function guess(FieldDescriptionInterface $fieldDescription): TypeGuess
     {
         switch ($fieldDescription->getMappingType()) {
             case 'boolean':
-                return new TypeGuess(BooleanType::class, [], Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(FieldDescriptionInterface::TYPE_BOOLEAN, [], Guess::HIGH_CONFIDENCE);
             case 'date':
-                return new TypeGuess(DatePickerType::class, [], Guess::HIGH_CONFIDENCE);
-
+                return new TypeGuess(FieldDescriptionInterface::TYPE_DATE, [], Guess::HIGH_CONFIDENCE);
             case 'decimal':
             case 'double':
-                return new TypeGuess(TextType::class, [], Guess::MEDIUM_CONFIDENCE);
+                return new TypeGuess(FieldDescriptionInterface::TYPE_STRING, [], Guess::MEDIUM_CONFIDENCE);
             case 'integer':
             case 'long':
-                return new TypeGuess(NumberType::class, [], Guess::MEDIUM_CONFIDENCE);
+                return new TypeGuess(FieldDescriptionInterface::TYPE_INTEGER, [], Guess::MEDIUM_CONFIDENCE);
             case 'string':
-                return new TypeGuess(TextType::class, [], Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(FieldDescriptionInterface::TYPE_STRING, [], Guess::HIGH_CONFIDENCE);
             case 'binary':
             case 'uri':
-                return new TypeGuess(TextType::class, [], Guess::MEDIUM_CONFIDENCE);
+                return new TypeGuess(FieldDescriptionInterface::TYPE_STRING, [], Guess::MEDIUM_CONFIDENCE);
             case ClassMetadata::MANY_TO_MANY:
             case 'referrers':
-                return new TypeGuess('doctrine_phpcr_many_to_many', [], Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(FieldDescriptionInterface::TYPE_MANY_TO_MANY, [], Guess::HIGH_CONFIDENCE);
 
             case ClassMetadata::MANY_TO_ONE:
             case 'parent':
-                return new TypeGuess('doctrine_phpcr_many_to_one', [], Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(FieldDescriptionInterface::TYPE_MANY_TO_ONE, [], Guess::HIGH_CONFIDENCE);
 
             case 'children':
-                return new TypeGuess('doctrine_phpcr_one_to_many', [], Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(FieldDescriptionInterface::TYPE_ONE_TO_MANY, [], Guess::HIGH_CONFIDENCE);
 
             case 'child':
-                return new TypeGuess('doctrine_phpcr_one_to_one', [], Guess::HIGH_CONFIDENCE);
+                return new TypeGuess(FieldDescriptionInterface::TYPE_ONE_TO_ONE, [], Guess::HIGH_CONFIDENCE);
         }
 
-        return new TypeGuess(TextType::class, [], Guess::LOW_CONFIDENCE);
+        return new TypeGuess(FieldDescriptionInterface::TYPE_STRING, [], Guess::LOW_CONFIDENCE);
     }
 }
